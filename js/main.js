8fffb9be49c9b25219954060b4dcb251a23741e9
mejigbedu <ayoolasuliton13@gmail.com>
2024-09-04 05:49:07 -0700

    Mimmie's Pot

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const showMenu =(toggleId, navId)=>{
+    const toggle = document.getElementById(toggleId);
+    const nav = document.getElementById(navId);
+
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show'); //javascript css to display nav menu
+        });
+    }
+};
+showMenu('nav-toggle', 'nav-menu');
+
+// Active and remove menu mobile
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show'); // when I click on each nav__link, remove te
+}
+navLink.forEach((n) => n.addEventListener('click', linkAction));
